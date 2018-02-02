28d427fdd07d96836083d59a7a959ccb59f1af8c
Ochir Darmaev <darmaev@rbsoft.ru>
2018-02-03 04:50:58 +0800

    add Wsdl yandex speller add teseract OCR

--- CheatingKeyboardSimulator/Form1.cs
+++ CheatingKeyboardSimulator/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CheatingKeyboardSimulator.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Tesseract;
 
 namespace CheatingKeyboardSimulator
 {
@@ -16,5 +18,35 @@ namespace CheatingKeyboardSimulator
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = @"C:\Users\ochir\Desktop\Снимок2.png";
+            textBox1.Text = GetResult(path, "rus");
+        }
+
+        private string GetResult(string pathFile, string laguage)
+        {
+            SpellServiceSoapClient client = new SpellServiceSoapClient("SpellServiceSoap");
+            
+            var img = new Bitmap(pathFile);
+            using (var ocr = new TesseractEngine("./tessdata", laguage, EngineMode.Default))
+            {
+                var page = ocr.Process(img);
+                var resStr = page.GetText();
+                var res = client.checkText(resStr,"ru",6,"plain");
+                foreach (var item in res)
+                {
+                    resStr.Replace(item.word, item.s.ToString());
+                }
+                return resStr;
+            }
+        }
     }
+
 }
+
